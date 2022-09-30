a91410845cebd6eb3188ffb55756582aba440240
Andrew Yaroshevych <ayaroshlvua@gmail.com>
2022-09-30 20:38:04 +0300

    Fixed capacity reservation

--- src/mystring.cpp
+++ src/mystring.cpp
@@ -178,10 +178,15 @@ void my_str_t::insert(size_t idx, const my_str_t &str) {
         throw std::out_of_range("Index is out of range");
     }
 
+    size_m += str.size_m;
+
+    if (size_m > capacity_m) {
+        reserve(calculate_capacity(size_m));
+    }
+
     memmove(data_m + idx + str.size_m, data_m + idx, size_m - idx + 1);
     memcpy(data_m + idx, str.data_m, str.size_m);
 
-    size_m += str.size_m;
     data_m[size_m] = '\0';
 }
 
