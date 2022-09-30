68133578b13278e04383cbe601537f438feee1ad
yurii-zinchuk <yurii.zinchuk@ucu.edu.ua>
2022-09-30 17:02:01 +0300

    performed merge from master

--- src/mystring.cpp
+++ src/mystring.cpp
@@ -1,97 +1,4 @@
-<<<<<<< HEAD
 
-#include <iostream>
-#include <cmath>
-#include <cstring>
-#include "./../include/mystring.h"
-
-int main(){
-    std::cout<<"Hello"<<std::endl;
-    const char stringa[]{"Hello"};
-    my_str_t test = my_str_t(10, 'a');
-    my_str_t test2 = my_str_t(stringa);
-    std::cout<<test.at(3)<<std::endl;
-    std::cout<<test2.at(1)<<std::endl;
-    std::cout<<test<<std::endl;
-    std::cout<<"Created successfully"<<std::endl;
-    return 0;
-}
-
-my_str_t::my_str_t(size_t size, char initial) {
-    //define memsize
-    size_t memsize = 16;
-    if (size>=memsize) {
-        memsize = static_cast<size_t>(pow(2, ceil(log2(size+1))));
-    }
-
-    //define capacity and size
-    capacity_m = memsize-1;
-    size_m = size;
-
-    //allocate memory and create string
-    data_m = new char[memsize];
-    for (int i = 0; i < size_m; ++i) {
-        *(data_m+i) = initial;
-    }
-    *(data_m+size_m) = '\0';
-}
-my_str_t::my_str_t(const char *cstr) {
-    //count size of c string
-    size_t csize = strlen(cstr);
-
-    //define memsize
-    size_t memsize = 16;
-    if (csize>=memsize) {
-        memsize = static_cast<size_t>(pow(2, ceil(log2(csize+1))));
-    }
-
-    //define capacity and size
-    capacity_m = memsize-1;
-    size_m = csize;
-
-    //allocate memory for our string and copy values
-    data_m = new char[memsize];
-    for (int i = 0; i < size_m; ++i) {
-        *(data_m+i) = *(cstr+i);
-    }
-    *(data_m+size_m) = '\0';
-}
-
-my_str_t::my_str_t(const std::string &str) {
-    size_t ssize = str.size();
-
-    size_t memsize = 16;
-    if (ssize>=memsize) {
-        memsize = static_cast<size_t>(pow(2, ceil(log2(ssize+1))));
-    }
-
-    capacity_m = memsize-1;
-    size_m = ssize;
-
-    data_m = new char[memsize];
-    for (int i = 0; i < size_m; ++i) {
-        *(data_m+i) = str.at(i);
-    }
-    *(data_m+size_m) = '\0';
-}
-
-my_str_t::my_str_t(const my_str_t &mystr) {
-    size_m = mystr.size_m;
-    capacity_m = mystr.capacity_m;
-
-    size_t memsize = capacity_m + 1;
-
-    data_m = new char[memsize];
-    for (int i = 0; i < size_m; ++i) {
-        *(data_m+i) = *(mystr.data_m+i);
-    }
-    *(data_m+size_m) = '\0';
-}
-
-my_str_t& my_str_t::operator=(const my_str_t& mystr) {
-    my_str_t temp{mystr};
-    swap(temp);
-=======
 //
 // Created by Andrew on 29.09.2022.
 //
@@ -181,14 +88,10 @@ my_str_t &my_str_t::operator=(const my_str_t &mystr) {
 
     data_m[size_m] = '\0';
 
->>>>>>> 94fa6111379020f7b671218a1553f25b7f10c123
     return *this;
 }
 
 char &my_str_t::at(size_t idx) {
-<<<<<<< HEAD
-    return *(data_m+idx);
-=======
     if (idx > size_m) {
         throw std::out_of_range("Index is out of range");
     }
@@ -212,14 +115,10 @@ size_t my_str_t::capacity() const noexcept {
 
 const char* my_str_t::c_str() const {
     return data_m;
->>>>>>> 94fa6111379020f7b671218a1553f25b7f10c123
 }
 
 my_str_t::~my_str_t() {
     delete[] data_m;
-<<<<<<< HEAD
-}
-=======
 }
 
 
@@ -227,4 +126,3 @@ std::ostream& operator<<(std::ostream& stream, const my_str_t& str) {
     stream << str.c_str();
     return stream;
 }
->>>>>>> 94fa6111379020f7b671218a1553f25b7f10c123
